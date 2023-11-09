b4e34b87bad68391ca0361105f678c55097ad80d
Luiz Quirino <luizfpq@gmail.com>
2023-11-09 18:45:30 -0300

    login funcionando

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 from app.routes.users import users
 #from app.routes.items import items
--- config.py
+++ config.py
@@ -3,3 +3,5 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'sua_chave_secreta_aqui'
